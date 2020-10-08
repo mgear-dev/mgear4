4b32ca261dd2d641eb3645d99f6d1fc29b7c9453
Jerome Drese <jdrese@gmail.com>
2020-10-08 23:32:51 +0200

    Add core.string tests

--- tests/test_mgear/test_core/test_string.py
+++ tests/test_mgear/test_core/test_string.py
@@ -1,7 +1,36 @@
+"""mgear.core.string test"""
 
+
+def tet_convert_RL_name(setup_path):
+    # mGear imports
+    from mgear.core.string import convert_RL_name
+    names = {"token_L": "token_R",
+             "token_L_token": "token_R_token",
+             "L_token": "R_token",
+             "L_token_L_token": "R_token_R_token"}
+    for item in names:
+        assert convert_RL_name(item) == names[item]
 
 def test_normalize(setup_path):
     # mGear imports
-    from mgear.core import string
+    from mgear.core.string import normalize
     value = "1234-mgear=string@normalize"
-    assert string.normalize(value) == "_1234-mgear_string_normalize"
+    assert normalize(value) == "_1234-mgear_string_normalize"
+    assert normalize(value, True) == "_1234_mgear_string_normalize"
+
+
+def test_remove_invalid_characters(setup_path):
+    # mGear imports
+    from mgear.core.string import remove_invalid_characters
+    value = "1234-mgear=string@normalize"
+    assert remove_invalid_characters(value) == "1234mgearstringnormalize"
+
+
+def test_replace_sharps_with_padding(setup_path):
+    # mGear imports
+    from mgear.core.string import replace_sharps_with_padding
+    assert replace_sharps_with_padding("value_###", 30) == "value_030"
+    assert replace_sharps_with_padding("value", 12) == "value12"
+    assert replace_sharps_with_padding("value", 2) == "value2"
+    assert replace_sharps_with_padding("value#", 2) == "value2"
+    assert replace_sharps_with_padding("value##", 2) == "value02"
