f4098eba4a95af31143419da6591941766b203a5
Jerome Drese <jdrese@gmail.com>
2020-10-19 23:43:21 +0200

    Add linear interpolate and fix length function

--- python/mgear/core/vector.py
+++ python/mgear/core/vector.py
@@ -12,8 +12,8 @@ def get_distance(input1, input2):
     """Returns distance between two vectors
 
     Args:
-        input1: MVector or PyNode.Transform or list
-        input2: MVector or PyNode.Transform or list
+        input1(MVector or PyNode.Transform or list): 3D Vector input A 
+        input2(MVector or PyNode.Transform or list): 3D Vector input B
 
     Returns:
         float: Distance value
@@ -21,18 +21,52 @@ def get_distance(input1, input2):
 
     # Return value
     distance = None
+    _vector = None
 
     # Handles MVector case
     if type(input1) and type(input2) == OpenMaya.MVector:
-        distance = input2 - input1
-    # Handles PyNode case
-    if type(input1) and type(input2) == pm.nodetypes.Transform:
+        _vector = input2 - input1
+    # Handles PyNodes case
+    elif type(input1) and type(input2) == pm.nodetypes.Transform:
         vector_1 = input1.getTranslation(space="world")
         vector_2 = input2.getTranslation(space="world")
-        distance = vector_2 - vector_1
+        _vector = vector_2 - vector_1
     # Handles list case
-    if type(input1) and type(input2) == list:
-        distance = math.sqrt(sum([(vec_a - vec_b) ** 2
-                                  for vec_a, vec_b in zip(input1, input2)]))
+    elif type(input1) and type(input2) == list:
+        # Calculates distance
+        return math.sqrt(sum([(vec_a - vec_b) ** 2
+            for vec_a, vec_b in zip(input1, input2)]))
 
+    distance = _vector.length()
     return distance
+
+
+def linear_interpolate(input1, input2, blend=0.5):
+    """Returns 3D vector linearly interpolated between 2 vector inputs
+
+    Args:
+        input1(MVector or PyNode.Transform or list): 3D Vector input A 
+        input2(MVector or PyNode.Transform or list): 3D Vector input B
+        blend: Ouput vector blend position.
+
+    Returns:
+        list: 3D vector interpolated position
+    """
+
+    # Return value
+    vector = None
+
+    # Handles PyNodes case
+    if type(input1) and type(input2) == pm.nodetypes.Transform:
+        input1 = input1.getTranslation()
+        input2 = input2.getTranslation()
+    # Handles list case
+    elif type(input1) and type(input2) == list:
+        input1 = OpenMaya.MVector(input1[0], input1[1], input1[2])
+        input2 = OpenMaya.MVector(input2[0], input2[1], input2[2])
+
+    vector = input2 - input1
+    vector *= blend
+    vector += input1
+
+    return vector
