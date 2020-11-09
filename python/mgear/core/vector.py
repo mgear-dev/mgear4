690641263aae61047b085f3512b7f18b5707777c
Jerome Drese <jdrese@gmail.com>
2020-11-09 22:37:10 +0100

    Add get_plane_binormal function
    Reorder functions as well

--- python/mgear/core/vector.py
+++ python/mgear/core/vector.py
@@ -43,6 +43,66 @@ def get_distance(input1, input2):
     return distance
 
 
+def get_plane_binormal(input1, input2, input3):
+    """Returns the binormal vector of a triangle (Defined by 3 positions)
+
+    Args:
+        input1(MVector or PyNode.Transform or list): 3D Vector input A
+        input2(MVector or PyNode.Transform or list): 3D Vector input B
+        input3(MVector or PyNode.Transform or list): 3D Vector input C
+
+    Returns:
+        maya.OpenMaya.MVector: 3D vector triangle binormal plane
+    """
+
+    # Get plane normal vector
+    normal_vector = get_plane_normal(input1, input2, input3)
+
+    # Calculate binormal vector
+    vector_a = input2 - input1
+    binormal = normal_vector ^ vector_a
+    binormal.normalize()
+
+    return binormal
+
+
+def get_plane_normal(input1, input2, input3):
+    """Returns the normal vector of a triangle (Defined by 3 positions)
+
+    Args:
+        input1(MVector or PyNode.Transform or list): 3D Vector input A
+        input2(MVector or PyNode.Transform or list): 3D Vector input B
+        input3(MVector or PyNode.Transform or list): 3D Vector input C
+
+    Returns:
+        maya.OpenMaya.MVector: 3D vector triangle normal plane
+    """
+
+    # Handles PyNodes case
+    if (isinstance(input1, pm.nodetypes.Transform)
+            and isinstance(input2, pm.nodetypes.Transform)
+            and isinstance(input3, pm.nodetypes.Transform)):
+        input1 = input1.getTranslation()
+        input2 = input2.getTranslation()
+        input3 = input3.getTranslation()
+    # Handles list case
+    elif (isinstance(input1, list) and isinstance(input2, list)
+          and isinstance(input3, list)):
+        input1 = OpenMaya.MVector(input1[0], input1[1], input1[2])
+        input2 = OpenMaya.MVector(input2[0], input2[1], input2[2])
+        input3 = OpenMaya.MVector(input3[0], input3[1], input3[2])
+
+    # Calculates normal vector
+    vector_a = input2 - input1
+    vector_b = input3 - input1
+    vector_a.normalize()
+    vector_b.normalize()
+    normal = vector_b ^ vector_a
+    normal.normalize()
+
+    return normal
+
+
 def linear_interpolate(input1, input2, blend=0.5):
     """Returns 3D vector linearly interpolated between 2 vector inputs
 
@@ -74,40 +134,3 @@ def linear_interpolate(input1, input2, blend=0.5):
     vector += input1
 
     return OpenMaya.MVector(vector[0], vector[1], vector[2])
-
-
-def get_plane_normal(input1, input2, input3):
-    """Returns the normal vector of a triangle (Defined by 3 positions).
-
-    Args:
-        input1(MVector or PyNode.Transform or list): 3D Vector input A
-        input2(MVector or PyNode.Transform or list): 3D Vector input B
-        input3(MVector or PyNode.Transform or list): 3D Vector input C
-
-    Returns:
-        maya.OpenMaya.MVector: 3D vector interpolated position
-    """
-
-    # Handles PyNodes case
-    if (isinstance(input1, pm.nodetypes.Transform)
-            and isinstance(input2, pm.nodetypes.Transform)
-            and isinstance(input3, pm.nodetypes.Transform)):
-        input1 = input1.getTranslation()
-        input2 = input2.getTranslation()
-        input3 = input3.getTranslation()
-    # Handles list case
-    elif (isinstance(input1, list) and isinstance(input2, list)
-          and isinstance(input3, list)):
-        input1 = OpenMaya.MVector(input1[0], input1[1], input1[2])
-        input2 = OpenMaya.MVector(input2[0], input2[1], input2[2])
-        input3 = OpenMaya.MVector(input3[0], input3[1], input3[2])
-
-    # Calculates normal vector
-    vector_a = input2 - input1
-    vector_b = input3 - input1
-    vector_a.normalize()
-    vector_b.normalize()
-    normal = vector_b ^ vector_a
-    normal.normalize()
-
-    return normal
--- tests/test_mgear/test_core/test_vector.py
+++ tests/test_mgear/test_core/test_vector.py
@@ -27,27 +27,19 @@ def test_get_distance(run_with_maya_pymel, setup_path):
     assert get_distance(v_1, v_2) == distance_value
 
 
-def test_linear_interpolate(run_with_maya_pymel, setup_path):
+def test_get_plane_binormal(run_with_maya_pymel, setup_path):
     # Maya imports
     from maya import OpenMaya
-    import pymel.core as pm
+
     # mGear imports
-    from mgear.core.vector import linear_interpolate
+    from mgear.core.vector import get_plane_binormal
 
-    _value = [2, 5, 8]
-
-    v_1 = [0, 0, 0]
-    v_2 = _value
-    result = linear_interpolate(v_1, v_2)
+    vector_a = OpenMaya.MVector(0, 0, 0)
+    vector_b = OpenMaya.MVector(-1, 0, 0)
+    vector_c = OpenMaya.MVector(0, 0, 1)
+    result = get_plane_binormal(vector_a, vector_b, vector_c)
     assert type(result) == OpenMaya.MVector
-    assert [result[0], result[1], result[2]] == [1, 2.5, 4]
-
-    pm.newFile(force=True)
-    v_1 = pm.createNode("transform")
-    v_2 = pm.createNode("transform")
-    v_2.translate.set(_value[0], _value[1], _value[2])
-    result = linear_interpolate(v_1, v_2)
-    assert [result[0], result[1], result[2]] == [1, 2.5, 4]
+    assert [result[0], result[1], result[2]] == [0, 0, -1]
 
 
 def test_get_plane_normal(run_with_maya_pymel, setup_path):
@@ -77,3 +69,26 @@ def test_get_plane_normal(run_with_maya_pymel, setup_path):
                               list(vector_b.getTranslation()),
                               list(vector_c.getTranslation()))
     assert [result[0], result[1], result[2]] == [0, -1, 0]
+
+
+def test_linear_interpolate(run_with_maya_pymel, setup_path):
+    # Maya imports
+    from maya import OpenMaya
+    import pymel.core as pm
+    # mGear imports
+    from mgear.core.vector import linear_interpolate
+
+    _value = [2, 5, 8]
+
+    v_1 = [0, 0, 0]
+    v_2 = _value
+    result = linear_interpolate(v_1, v_2)
+    assert type(result) == OpenMaya.MVector
+    assert [result[0], result[1], result[2]] == [1, 2.5, 4]
+
+    pm.newFile(force=True)
+    v_1 = pm.createNode("transform")
+    v_2 = pm.createNode("transform")
+    v_2.translate.set(_value[0], _value[1], _value[2])
+    result = linear_interpolate(v_1, v_2)
+    assert [result[0], result[1], result[2]] == [1, 2.5, 4]
