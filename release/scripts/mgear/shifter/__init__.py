8192ef4dba438785c61ee79963b91a12cf040ed5
Miquel Campos <miquel.campos@gmail.com>
2021-11-10 11:27:26 +0900

    fix format PEP8

--- release/scripts/mgear/shifter/__init__.py
+++ release/scripts/mgear/shifter/__init__.py
@@ -346,7 +346,8 @@ class Rig(object):
             self.model, "jnt_vis", "bool", True)
         # adding the always draw shapes on top to global attribute
         if versions.current() >= 20220000:
-            self.ctlXRay_att = attribute.addAttribute(self.model, "ctl_x_ray", "bool", False)
+            self.ctlXRay_att = attribute.addAttribute(
+                self.model, "ctl_x_ray", "bool", False)
 
         self.qsA_att = attribute.addAttribute(
             self.model, "quickselA", "string", "")
@@ -568,7 +569,8 @@ class Rig(object):
             oShape.isHistoricallyInteresting.set(False)
             # connecting the always draw shapes on top to global attribute
             if versions.current() >= 20220000:
-                pm.connectAttr(self.ctlXRay_att, oShape.attr("alwaysDrawOnTop"))
+                pm.connectAttr(
+                    self.ctlXRay_att, oShape.attr("alwaysDrawOnTop"))
 
         # set controller tag
         if versions.current() >= 201650:
