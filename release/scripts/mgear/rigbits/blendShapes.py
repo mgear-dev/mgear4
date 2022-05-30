6f9588b0825f61c246f43c8b5e6e2b0df4f00c68
miquelcampos <miquel.campos@gmail.com>
2022-05-30 09:58:38 +0900

    Rigbits: Move existing blendshape node to the front of chain closes #128

--- release/scripts/mgear/rigbits/blendShapes.py
+++ release/scripts/mgear/rigbits/blendShapes.py
@@ -17,8 +17,11 @@ def getBlendShape(obj):
         obj = pm.PyNode(obj)
 
     try:
-        if (pm.nodeType(obj.getShape())
-                in ["mesh", "nurbsSurface", "nurbsCurve"]):
+        if pm.nodeType(obj.getShape()) in [
+            "mesh",
+            "nurbsSurface",
+            "nurbsCurve",
+        ]:
             blendShape = pm.listHistory(obj.getShape(), type="blendShape")[0]
     except Exception:
         blendShape = None
@@ -26,13 +29,38 @@ def getBlendShape(obj):
     return blendShape
 
 
-def connectWithBlendshape(mesh, bst, wgt=1.0):
+def blendshape_foc(deformed_obj):
+    """Move existing blendshape node to the front of chain
+
+    Args:
+        deformed_obj (PyNode): object with deformation history including a
+                blendshape node
+    """
+    meshShape = deformed_obj.getShape()
+
+    history = pm.listHistory(
+        meshShape, gl=True, pdo=True, lf=True, f=False, il=2
+    )
+    blendShape_node = None
+    deformers = []
+    for h in history:
+        object_type = pm.objectType(h)
+        if object_type == "blendShape":
+            blendShape_node = h
+        else:
+            deformers.append(h)
+    if blendShape_node:
+        pm.reorderDeformers(*deformers, blendShape_node, meshShape)
+
+
+def connectWithBlendshape(mesh, bst, wgt=1.0, ffoc=False):
     """Connect 2 geometries using blendshape node
 
     Args:
         mesh (PyNode): The Object to apply the blendshape target
         bst (PyNode): The Blendshape target
         wgt (float, optional): Description
+        ffoc (bool, optional): Force Front of Chain. will move the blendshape node after creation
 
     Returns:
         PyNode: The blenshape node
@@ -48,10 +76,18 @@ def connectWithBlendshape(mesh, bst, wgt=1.0):
         bsnode.attr(bst.name()).set(wgt)
         bs = bsnode
     else:
-        bs = pm.blendShape(bst,
-                           mesh,
-                           name="_".join([mesh.name(), "blendShape"]),
-                           foc=True,
-                           w=[(0, 1.0)])
+        if ffoc:
+            foc = False
+        else:
+            foc = True
+        bs = pm.blendShape(
+            bst,
+            mesh,
+            name="_".join([mesh.name(), "blendShape"]),
+            foc=foc,
+            w=[(0, 1.0)],
+        )
+        if ffoc:
+            blendshape_foc(mesh)
 
     return bs
