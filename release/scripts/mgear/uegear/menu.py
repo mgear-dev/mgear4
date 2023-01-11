a015dcabc06e04f9fdec6e697de45fc76a6ad495
miquelcampos <miquel.campos@gmail.com>
2023-01-11 17:26:24 +0900

    adding UE5 icon to the menu #147

--- release/scripts/mgear/uegear/menu.py
+++ release/scripts/mgear/uegear/menu.py
@@ -8,18 +8,23 @@ menuID = "ueGear"
 
 
 def install():
-	""" Installs ueGear sub-menu
-	"""
+    """Installs ueGear sub-menu"""
 
-	cmds.setParent(mgear.menu_id, menu=True)
-	cmds.menuItem(divider=True)
-	commands = (
-		("Import Selected Assets from Unreal", str_import_selected_assets_from_unreal),
-		("Export Selected Assets to Unreal", str_export_selected_assets_to_unreal),
-		("-----", None),
-	)
+    cmds.setParent(mgear.menu_id, menu=True)
+    cmds.menuItem(divider=True)
+    commands = (
+        (
+            "Import Selected Assets from Unreal",
+            str_import_selected_assets_from_unreal,
+        ),
+        (
+            "Export Selected Assets to Unreal",
+            str_export_selected_assets_to_unreal,
+        ),
+        ("-----", None),
+    )
 
-	mgear.menu.install(menuID, commands)
+    mgear.menu.install(menuID, commands, image="UE5.svg")
 
 
 str_import_selected_assets_from_unreal = """
