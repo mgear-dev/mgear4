2651469e8dd7f063c20601ad8d43dd49a748fa61
Tomi Poveda <tpovedatd@gmail.com>
2023-01-11 19:50:16 +0100

    Added commands.py module + Now FBX export process is safer #147

--- release/scripts/mgear/uegear/bridge.py
+++ release/scripts/mgear/uegear/bridge.py
@@ -18,7 +18,7 @@ import pymel.core as pm
 import maya.cmds as cmds
 
 from mgear.core import pyFBX
-from mgear.uegear import log, utils, tag, io, ioutils
+from mgear.uegear import log, utils, tag, io
 
 logger = log.uegear_logger
 
@@ -136,117 +136,6 @@ class UeGearBridge(object):
         self._is_executing = False
 
         return response
-
-    # ==================================================================================================================
-    # ASSETS
-    # ==================================================================================================================
-
-    def import_selected_assets_from_unreal(self):
-        """
-        Import current selected Content Browser Unreal assets into the Maya scene.
-        """
-
-        temp_folder = tempfile.gettempdir()
-        asset_export_datas = self.execute(
-            'export_selected_assets', parameters={'directory': temp_folder}).get('ReturnValue', list())
-
-        for asset_export_data in asset_export_datas:
-            fbx_file = asset_export_data.get('fbx_file', None)
-            if not fbx_file or not os.path.isfile(fbx_file):
-                continue
-            imported_nodes = utils.import_fbx(fbx_file)
-            transform_nodes = cmds.ls(imported_nodes, type='transform')
-            for transform_node in transform_nodes:
-                asset_type = asset_export_data.get('asset_type', '')
-                asset_name = asset_export_data.get('name', '')
-                asset_path = asset_export_data.get('path', '')
-                if asset_type:
-                    tag.apply_tag(transform_node, tag.TAG_ASSET_TYPE_ATTR_NAME, asset_type)
-                else:
-                    tag.auto_tag(transform_node)
-                if asset_name:
-                    tag.apply_tag(transform_node, tag.TAG_ASSET_NAME_ATTR_NAME, asset_name)
-                if asset_path:
-                    tag.apply_tag(transform_node, tag.TAG_ASSET_PATH_ATTR_NAME, asset_path)
-
-    def export_selected_assets_to_unreal(self, export_directory=None, export_in_original_path=True):
-        """
-        Exports current selected assets in Maya scene into Unreal Engine Content Browser.
-        """
-
-        temp_folder = tempfile.gettempdir()
-
-        nodes_to_export = cmds.ls(sl=True, long=True)
-        objects_map = io.exportable_assets(nodes=nodes_to_export)
-        if not objects_map:
-            return False
-
-        static_meshes = objects_map.get(tag.TagTypes.StaticMesh, list())
-        if not static_meshes:
-            logger.warning('No static meshes to update')
-            return False
-
-        content_path = self.execute('project_content_directory').get('ReturnValue', '')
-        if not content_path or not os.path.isdir(content_path):
-            logger.warning('Was not possible to retrieve current Unreal project content path')
-            return False
-
-        for static_mesh in static_meshes:
-            asset_path = tag.tag_values(tag_name=tag.TAG_ASSET_PATH_ATTR_NAME, nodes=[static_mesh])
-            asset_path = asset_path[0] if asset_path else ''
-            asset_exists = self.execute(
-                'does_asset_exist', parameters={'asset_path': asset_path}).get('ReturnValue', False)
-            if not asset_exists:
-                logger.warning('Asset "{}" does not exists within current Unreal Project!'.format(asset_path))
-                continue
-
-            asset_file_name = os.path.basename(asset_path).split('.')[0]
-            uasset_file_name = asset_file_name + '.uasset'
-            content_uasset_path = utils.join_path(content_path, os.path.dirname(asset_path).replace('/Game/', '/'), uasset_file_name)
-            if not os.path.isfile(content_uasset_path):
-                logger.warning('.uasset file was not found: "{}"'.format(content_uasset_path))
-                continue
-
-            export_file_name = '{}.fbx'.format(asset_file_name)
-            if not export_directory and export_in_original_path:
-                asset_export_path = self.execute(
-                    'asset_export_path', parameters={'asset_path': asset_path}).get('ReturnValue', '')
-                export_file_name = '{}.fbx'.format(os.path.basename(asset_export_path).split('.')[0])
-            else:
-                if not export_directory:
-                    asset_export_path = utils.join_path(temp_folder, export_file_name)
-                else:
-                    asset_export_path = utils.join_path(export_directory, export_file_name)
-
-            if export_directory or export_in_original_path:
-                asset_export_directory = os.path.dirname(asset_export_path)
-                if not os.path.isdir(asset_export_directory):
-                    logger.info('Export directory does not exist, trying to create it: {}'.format(asset_export_directory))
-                    result = utils.create_folder(asset_export_directory)
-                    if not result:
-                        logger.warning(
-                            'Was not possible to create original export path: "{}" | temp folder will be used instead...'.format(
-                                asset_export_directory))
-                        asset_export_path = utils.join_path(temp_folder, '{}.fbx'.format(asset_file_name))
-            asset_export_directory = os.path.dirname(asset_export_path)
-
-            fbx_file_path = ioutils.export_static_mesh(asset_export_directory, static_mesh, file_name=export_file_name)
-            if not os.path.isfile(fbx_file_path):
-                logger.warning('Something went wrong while exporting asset FBX file: "{}"'.format(fbx_file_path))
-                continue
-
-            import_options = {'destination_name': asset_file_name, 'replace_existing': True, 'save': False}
-            result = self.execute(
-                'import_static_mesh', parameters={
-                    'fbx_file': fbx_file_path,
-                    'import_path': os.path.dirname(content_uasset_path),
-                    'import_options': str(import_options)
-                }).get('ReturnValue', False)
-            if not result:
-                logger.warning('Was not possible to export asset: {}'.format(static_mesh))
-                continue
-
-        return True
 
     # ==================================================================================================================
     # TRANSFORM COMMANDS
--- release/scripts/mgear/uegear/ioutils.py
+++ release/scripts/mgear/uegear/ioutils.py
@@ -1,4 +1,14 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Module that contains import and export utility functions.
+"""
+
+from __future__ import print_function, division, absolute_import
+
 import os
+import shutil
 
 import maya.cmds as cmds
 
@@ -10,13 +20,23 @@ logger = log.uegear_logger
 
 @utils.keep_selection_decorator
 def export_static_mesh(export_path, static_mesh, file_name=None):
+
 	cmds.select(static_mesh)
 	output_file = file_name
 	if not file_name:
 		output_file = '{}.fbx'.format(static_mesh.replace(':', '_').split('|')[-1])
 	if not output_file.endswith('.fbx'):
 		output_file = output_file + '.fbx'
-	export_path = utils.join_path(export_path, output_file)
+	final_export_path = utils.join_path(export_path, output_file)
+
+	# if FBX already exists, to make the export process safer, we export the FBX using a temporal name, so we only
+	# overwrite the original FBX if the FBX export was completed successfully.
+	export_path = final_export_path
+	if os.path.isfile(final_export_path):
+		_export_file_name = os.path.basename(final_export_path)
+		_file_name, _file_extension = os.path.splitext(_export_file_name)
+		export_path = utils.join_path(os.path.dirname(final_export_path), '{}_uegear_temp{}'.format(_file_name, _file_extension))
+
 	pyFBX.FBXExportBakeComplexStart(v=1)
 	pyFBX.FBXExportBakeComplexEnd(v=1)
 	pyFBX.FBXExportFileVersion(v='FBX201400')
@@ -37,15 +57,24 @@ def export_static_mesh(export_path, static_mesh, file_name=None):
 	pyFBX.FBXExportTangents(v=False)
 	pyFBX.FBXExportTriangulate(v=True)
 	pyFBX.FBXExportInputConnections(v=False)
-	pyFBX.FBXExport(f=export_path, s=True)
 
-	if os.path.isfile(export_path):
-		logger.info('\nExported {}'.format(export_path))
-		return export_path
+	valid_export = True
+	try:
+		pyFBX.FBXExport(f=export_path, s=True)
+	except RuntimeError as exc:
+		logger.error('Something went wrong while exporting FBX in following path: "{}" | {}'.format(export_path, exc))
+		valid_export = False
 
-	return ''
+	if not valid_export or not os.path.isfile(export_path):
+		logger.warning('Was not possible to export FBX!')
+		return ''
 
+	if export_path != final_export_path:
+		shutil.move(export_path, final_export_path)
+		if not os.path.isfile(final_export_path):
+			logger.error('Something went wrong while renaming exported FBX file from "{}" to "{}"'.format(
+				export_path, final_export_path))
+			return ''
 
-
-
-
+	logger.info('Exported {}'.format(final_export_path))
+	return final_export_path
