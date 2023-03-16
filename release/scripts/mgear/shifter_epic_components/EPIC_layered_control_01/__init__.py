9dae335dee929a6c13dc1743974e35fb29e66dd2
miquelcampos <miquel.campos@gmail.com>
2023-03-16 14:10:39 +0900

    EPIC_layered_control_01 closes #226

---
+++ release/scripts/mgear/shifter_epic_components/EPIC_layered_control_01/__init__.py
@@ -0,0 +1,317 @@
+"""
+MIT License
+
+Copyright (c) 2011-2018 Jeremie Passerin, Miquel Campos - from 2018 The mGear-Dev Organization
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+from mgear.shifter import component
+
+from mgear.core import attribute
+from mgear.core import transform
+from mgear.core import primitive
+from mgear.core import applyop
+from mgear.core import node
+import pymel.core as pm
+
+#############################################
+# COMPONENT
+#############################################
+
+
+class Component(component.Main):
+    """Shifter component Class"""
+
+    # =====================================================
+    # OBJECTS
+    # =====================================================
+    def addObjects(self):
+        """Add all the objects needed to create the component."""
+
+        if self.settings["neutralRotation"]:
+            t = transform.getTransformFromPos(self.guide.pos["root"])
+        else:
+            t = self.guide.tra["root"]
+            if self.settings["mirrorBehaviour"] and self.negate:
+                scl = [1, 1, -1]
+            else:
+                scl = [1, 1, 1]
+            t = transform.setMatrixScale(t, scl)
+        if self.settings["joint"] and self.settings["leafJoint"]:
+            pm.displayInfo("Skipping ctl creation, just leaf joint")
+            self.have_ctl = False
+        else:
+            self.have_ctl = True
+            self.ik_cns = primitive.addTransform(
+                self.root, self.getName("ik_cns"), t
+            )
+            self.rbf_ctl = []
+            rbf_ctl_parent = self.ik_cns
+            size_inc = (self.size / 2) / self.settings["rbf_layers"]
+            size_add = size_inc
+            for i in range(self.settings["rbf_layers"]):
+
+                rbf_ctl = self.addCtl(
+                    rbf_ctl_parent,
+                    "rbf{}_ctl".format(str(i).zfill(3)),
+                    t,
+                    self.color_fk,
+                    "cubewithpeak",
+                    w=self.settings["ctlSize"] * (self.size / 4) + size_add,
+                    h=self.settings["ctlSize"] * (self.size / 4) + size_add,
+                    d=self.settings["ctlSize"] * (self.size / 4) + size_add,
+                    tp=self.parentCtlTag,
+                    guide_loc_ref="root",
+                    add_2_grp=False,
+                )
+                size_add += size_inc
+                self.rbf_ctl.append(rbf_ctl)
+                rbf_ctl_parent = rbf_ctl
+
+            self.rbf_ref = primitive.addTransform(
+                self.ik_cns, self.getName("rbf_ref"), t
+            )
+
+            self.ctl_npo = primitive.addTransform(
+                self.ik_cns, self.getName("npo"), t
+            )
+            self.jnt_lvl = primitive.addTransform(
+                self.ctl_npo, self.getName("jnt_lvl"), t
+            )
+
+            self.ctl = self.addCtl(
+                self.ctl_npo,
+                "ctl",
+                t,
+                self.color_ik,
+                self.settings["icon"],
+                w=self.settings["ctlSize"] * self.size,
+                h=self.settings["ctlSize"] * self.size,
+                d=self.settings["ctlSize"] * self.size,
+                tp=self.parentCtlTag,
+                guide_loc_ref="root",
+            )
+
+            # we need to set the rotation order before lock any rotation axis
+            if self.settings["k_ro"]:
+                rotOderList = ["XYZ", "YZX", "ZXY", "XZY", "YXZ", "ZYX"]
+                attribute.setRotOrder(
+                    self.ctl, rotOderList[self.settings["default_rotorder"]]
+                )
+
+            params = [
+                s
+                for s in [
+                    "tx",
+                    "ty",
+                    "tz",
+                    "ro",
+                    "rx",
+                    "ry",
+                    "rz",
+                    "sx",
+                    "sy",
+                    "sz",
+                ]
+                if self.settings["k_" + s]
+            ]
+            attribute.setKeyableAttributes(self.ctl, params)
+        if self.settings["joint"]:
+            # TODO WIP: add new attr for seeting leaf joint + not build objcts
+            if self.settings["leafJoint"]:
+                self.jnt_pos.append([t, 0, None, self.settings["uniScale"]])
+            else:
+                self.jnt_pos.append(
+                    [self.jnt_lvl, 0, None, self.settings["uniScale"]]
+                )
+
+    def addAttributes(self):
+        # self.pivotVis_att = self.addAnimParam(
+        #     "pivot_vis", "Pivot Ctl Vis", "bool", False, uihost=self.ctl
+        # )
+
+        self.ctlVis_att = self.addAnimParam("ctl_vis", "Ctl Vis", "bool", True)
+
+        self.rbfVis_att = []
+        for i in range(self.settings["rbf_layers"]):
+            rbfVis_att = self.addAnimParam(
+                "rbf{}_ctl_vis".format(str(i).zfill(3)),
+                "RBF {} Ctl Vis".format(str(i).zfill(3)),
+                "bool",
+                False,
+            )
+            self.rbfVis_att.append(rbfVis_att)
+
+        # rbf transform multipliers
+        self.RBFMult_att = self.addAnimParam("RBFMult", "RBF Mult", "float", 1)
+
+        # Remap multipliers
+        self.remap_tx_att = self.addAnimParam(
+            "remapTX", "Remap TX", "float", 1
+        )
+        self.remap_ty_att = self.addAnimParam(
+            "remapTY", "Remap TY", "float", 1
+        )
+        self.remap_tz_att = self.addAnimParam(
+            "remapTZ", "Remap TZ", "float", 1
+        )
+
+        self.remap_rx_att = self.addAnimParam(
+            "remapRX", "Remap RX", "float", 1
+        )
+        self.remap_ry_att = self.addAnimParam(
+            "remapRY", "Remap RY", "float", 1
+        )
+        self.remap_rz_att = self.addAnimParam(
+            "remapRZ", "Remap RZ", "float", 1
+        )
+
+        self.remap_sx_att = self.addAnimParam(
+            "remapSX", "Remap SX", "float", 1
+        )
+        self.remap_sy_att = self.addAnimParam(
+            "remapSY", "Remap SY", "float", 1
+        )
+        self.remap_sz_att = self.addAnimParam(
+            "remapSZ", "Remap SZ", "float", 1
+        )
+
+        # Ref
+        if self.have_ctl:
+            if self.settings["ikrefarray"]:
+                ref_names = self.get_valid_alias_list(
+                    self.settings["ikrefarray"].split(",")
+                )
+                if len(ref_names) > 1:
+                    self.ikref_att = self.addAnimEnumParam(
+                        "ikref", "Ik Ref", 0, ref_names
+                    )
+
+        # SETUP param
+        # ctl transform multipliers
+        self.ctlTransMult_att = self.addSetupParam(
+            "ctlTransMult",
+            "Ctl Translate Mult",
+            "float",
+            self.settings["ctlTransMult"],
+        )
+        self.ctlRotMult_att = self.addSetupParam(
+            "ctlRotMult",
+            "Ctl Rotate Mult",
+            "float",
+            self.settings["ctlRotMult"],
+        )
+        self.ctlSclMult_att = self.addSetupParam(
+            "ctlSclMult",
+            "Ctl Scale Mult",
+            "float",
+            self.settings["ctlSclMult"],
+        )
+
+    def addOperators(self):
+        # visibilities
+        for shp in self.ctl.getShapes():
+            pm.connectAttr(self.ctlVis_att, shp.attr("visibility"))
+        for i in range(self.settings["rbf_layers"]):
+            for shp in self.rbf_ctl[i].getShapes():
+                pm.connectAttr(self.rbfVis_att[i], shp.attr("visibility"))
+
+        # RBF reference transform
+        applyop.gear_mulmatrix_op(
+            self.rbf_ctl[-1].attr("worldMatrix"),
+            self.rbf_ref.attr("parentInverseMatrix[0]"),
+            self.rbf_ref,
+        )
+
+        # ctl npo RBF
+        node.createMulNode(
+            [self.rbf_ref.tx, self.rbf_ref.ty, self.rbf_ref.tz],
+            [self.RBFMult_att] * 3,
+            [self.ctl_npo.tx, self.ctl_npo.ty, self.ctl_npo.tz],
+        )
+        node.createMulNode(
+            [self.rbf_ref.rx, self.rbf_ref.ry, self.rbf_ref.rz],
+            [self.RBFMult_att] * 3,
+            [self.ctl_npo.rx, self.ctl_npo.ry, self.ctl_npo.rz],
+        )
+
+        # ctl multiplier to jnt
+        # translate
+        mult_node = node.createMulNode(
+            [self.ctl.tx, self.ctl.ty, self.ctl.tz],
+            [self.ctlTransMult_att] * 3,
+        )
+        node.createMulNode(
+            [mult_node.outputX, mult_node.outputY, mult_node.outputZ],
+            [self.remap_tx_att, self.remap_ty_att, self.remap_tz_att],
+            [self.jnt_lvl.tx, self.jnt_lvl.ty, self.jnt_lvl.tz],
+        )
+
+        # rotate
+        mult_node = node.createMulNode(
+            [self.ctl.rx, self.ctl.ry, self.ctl.rz],
+            [self.ctlRotMult_att] * 3,
+        )
+        node.createMulNode(
+            [mult_node.outputX, mult_node.outputY, mult_node.outputZ],
+            [self.remap_rx_att, self.remap_ry_att, self.remap_rz_att],
+            [self.jnt_lvl.rx, self.jnt_lvl.ry, self.jnt_lvl.rz],
+        )
+
+        # scale
+        mult_node = node.createMulNode(
+            [self.ctl.sx, self.ctl.sy, self.ctl.sz],
+            [self.ctlSclMult_att] * 3,
+        )
+        node.createMulNode(
+            [mult_node.outputX, mult_node.outputY, mult_node.outputZ],
+            [self.remap_sx_att, self.remap_sy_att, self.remap_sz_att],
+            [self.jnt_lvl.sx, self.jnt_lvl.sy, self.jnt_lvl.sz],
+        )
+
+    # =====================================================
+    # CONNECTOR
+    # =====================================================
+
+    def setRelation(self):
+        """Set the relation beetween object from guide to rig"""
+        if self.have_ctl:
+            self.relatives["root"] = self.jnt_lvl
+            self.controlRelatives["root"] = self.ctl
+
+        else:
+            self.relatives["root"] = self.root
+            self.controlRelatives["root"] = None
+
+        if self.settings["joint"]:
+            self.jointRelatives["root"] = 0
+        self.aliasRelatives["root"] = "ctl"
+
+    def addConnection(self):
+        """Add more connection definition to the set"""
+        self.connections["standard"] = self.connect_standard
+        self.connections["orientation"] = self.connect_orientation
+
+    def connect_standard(self):
+        """standard connection definition for the component"""
+        self.connect_standardWithSimpleIkRef()
+
+    def connect_orientation(self):
+        """Orient connection definition for the component"""
+        self.connect_orientCns()
---
+++ release/scripts/mgear/shifter_epic_components/EPIC_layered_control_01/guide.py
@@ -0,0 +1,435 @@
+"""
+MIT License
+
+Copyright (c) 2011-2018 Jeremie Passerin, Miquel Campos - from 2018 The mGear-Dev Organization
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from functools import partial
+import pymel.core as pm
+
+from mgear.shifter.component import guide
+from mgear.core import transform, pyqt, attribute
+from mgear.vendor.Qt import QtWidgets, QtCore
+
+from maya.app.general.mayaMixin import MayaQWidgetDockableMixin
+from maya.app.general.mayaMixin import MayaQDockWidget
+
+from . import settingsUI as sui
+
+
+# guide info
+AUTHOR = "Miquel Campos"
+URL = "www.mcsgear.com"
+EMAIL = ""
+VERSION = [1, 0, 0]
+TYPE = "EPIC_layered_control_01"
+NAME = "control"
+DESCRIPTION = "Control with multiple layered input controls to connect RBF or other type of driver."
+
+##########################################################
+# CLASS
+##########################################################
+
+
+class Guide(guide.ComponentGuide):
+    """Component Guide Class"""
+
+    compType = TYPE
+    compName = NAME
+    description = DESCRIPTION
+
+    author = AUTHOR
+    url = URL
+    email = EMAIL
+    version = VERSION
+
+    connectors = ["orientation"]
+
+    # =====================================================
+    ##
+    # @param self
+    def postInit(self):
+        self.save_transform = ["root", "sizeRef"]
+
+    # =====================================================
+    # Add more object to the object definition list.
+    # @param self
+    def addObjects(self):
+
+        self.root = self.addRoot()
+        vTemp = transform.getOffsetPosition(self.root, [0, 0, 1])
+        self.sizeRef = self.addLoc("sizeRef", self.root, vTemp)
+        pm.delete(self.sizeRef.getShapes())
+        attribute.lockAttribute(self.sizeRef)
+
+    # =====================================================
+    # Add more parameter to the parameter definition list.
+    # @param self
+    def addParameters(self):
+
+        self.pIcon = self.addParam("icon", "string", "cube")
+
+        self.pIkRefArray = self.addParam("ikrefarray", "string", "")
+
+        self.pJoint = self.addParam("joint", "bool", False)
+        self.pLeafJoint = self.addParam("leafJoint", "bool", False)
+        self.pJoint = self.addParam("uniScale", "bool", False)
+        self.pRBFlayers = self.addParam("rbf_layers", "long", 1, None, None)
+
+        for s in ["tx", "ty", "tz", "ro", "rx", "ry", "rz", "sx", "sy", "sz"]:
+            self.addParam("k_" + s, "bool", True)
+
+        self.pDefault_RotOrder = self.addParam(
+            "default_rotorder", "long", 0, 0, 5
+        )
+        self.pNeutralRotation = self.addParam("neutralRotation", "bool", True)
+        self.pMirrorBehaviour = self.addParam("mirrorBehaviour", "bool", False)
+        self.pCtlSize = self.addParam("ctlSize", "double", 1, None, None)
+
+        self.pCtlTransMult = self.addParam(
+            "ctlTransMult", "double", 1, None, None
+        )
+        self.pCtlRotMult = self.addParam("ctlRotMult", "double", 1, None, None)
+        self.pCtlSclMult = self.addParam("ctlSclMult", "double", 1, None, None)
+
+        self.pUseIndex = self.addParam("useIndex", "bool", False)
+        self.pParentJointIndex = self.addParam(
+            "parentJointIndex", "long", -1, None, None
+        )
+
+        return
+
+    def postDraw(self):
+        "Add post guide draw elements to the guide"
+        size = pm.xform(self.root, q=True, ws=True, scale=True)[0]
+        self.add_ref_axis(
+            self.root,
+            self.root.neutralRotation,
+            inverted=True,
+            width=0.5 / size,
+        )
+        self.add_ref_joint(
+            self.root, [self.root.leafJoint, self.root.joint], width=0.1 / size
+        )
+
+
+##########################################################
+# Setting Page
+##########################################################
+
+
+class settingsTab(QtWidgets.QDialog, sui.Ui_Form):
+    """The Component settings UI"""
+
+    def __init__(self, parent=None):
+        super(settingsTab, self).__init__(parent)
+        self.setupUi(self)
+
+
+class componentSettings(MayaQWidgetDockableMixin, guide.componentMainSettings):
+    """Create the component setting window"""
+
+    def __init__(self, parent=None):
+        self.toolName = TYPE
+        # Delete old instances of the componet settings window.
+        pyqt.deleteInstances(self, MayaQDockWidget)
+        self.iconsList = [
+            "arrow",
+            "circle",
+            "compas",
+            "cross",
+            "crossarrow",
+            "cube",
+            "cubewithpeak",
+            "cylinder",
+            "diamond",
+            "flower",
+            "null",
+            "pyramid",
+            "sphere",
+            "square",
+        ]
+
+        super(self.__class__, self).__init__(parent=parent)
+        self.settingsTab = settingsTab()
+
+        self.setup_componentSettingWindow()
+        self.create_componentControls()
+        self.populate_componentControls()
+        self.create_componentLayout()
+        self.create_componentConnections()
+
+    def setup_componentSettingWindow(self):
+        self.mayaMainWindow = pyqt.maya_main_window()
+
+        self.setObjectName(self.toolName)
+        self.setWindowFlags(QtCore.Qt.Window)
+        self.setWindowTitle(TYPE)
+        self.resize(350, 520)
+
+    def create_componentControls(self):
+        return
+
+    def populate_componentControls(self):
+        """Populate Controls
+
+        Populate the controls values from the custom attributes of the
+        component.
+
+        """
+        # populate tab
+        self.tabs.insertTab(1, self.settingsTab, "Component Settings")
+
+        # populate component settings
+
+        self.populateCheck(self.settingsTab.joint_checkBox, "joint")
+        self.enable_leaf_joint()
+        self.populateCheck(self.settingsTab.leafJoint_checkBox, "leafJoint")
+        self.populateCheck(self.settingsTab.uniScale_checkBox, "uniScale")
+        self.populateCheck(
+            self.settingsTab.neutralRotation_checkBox, "neutralRotation"
+        )
+        self.populateCheck(
+            self.settingsTab.mirrorBehaviour_checkBox, "mirrorBehaviour"
+        )
+        self.settingsTab.ctlSize_doubleSpinBox.setValue(
+            self.root.attr("ctlSize").get()
+        )
+        self.settingsTab.rbfLayers_spinBox.setValue(
+            self.root.attr("rbf_layers").get()
+        )
+
+        self.settingsTab.ctlTransMult_doubleSpinBox.setValue(
+            self.root.attr("ctlTransMult").get()
+        )
+        self.settingsTab.ctlRotMult_doubleSpinBox.setValue(
+            self.root.attr("ctlRotMult").get()
+        )
+        self.settingsTab.ctlSclMult_doubleSpinBox.setValue(
+            self.root.attr("ctlSclMult").get()
+        )
+
+        sideIndex = self.iconsList.index(self.root.attr("icon").get())
+        self.settingsTab.controlShape_comboBox.setCurrentIndex(sideIndex)
+
+        self.populateCheck(self.settingsTab.tx_checkBox, "k_tx")
+        self.populateCheck(self.settingsTab.ty_checkBox, "k_ty")
+        self.populateCheck(self.settingsTab.tz_checkBox, "k_tz")
+        self.populateCheck(self.settingsTab.rx_checkBox, "k_rx")
+        self.populateCheck(self.settingsTab.ry_checkBox, "k_ry")
+        self.populateCheck(self.settingsTab.rz_checkBox, "k_rz")
+        self.populateCheck(self.settingsTab.ro_checkBox, "k_ro")
+        self.populateCheck(self.settingsTab.sx_checkBox, "k_sx")
+        self.populateCheck(self.settingsTab.sy_checkBox, "k_sy")
+        self.populateCheck(self.settingsTab.sz_checkBox, "k_sz")
+
+        self.settingsTab.ro_comboBox.setCurrentIndex(
+            self.root.attr("default_rotorder").get()
+        )
+
+        ikRefArrayItems = self.root.attr("ikrefarray").get().split(",")
+        for item in ikRefArrayItems:
+            self.settingsTab.ikRefArray_listWidget.addItem(item)
+
+        # populate connections in main settings
+        for cnx in Guide.connectors:
+            self.mainSettingsTab.connector_comboBox.addItem(cnx)
+        cBox = self.mainSettingsTab.connector_comboBox
+        self.connector_items = [cBox.itemText(i) for i in range(cBox.count())]
+        currentConnector = self.root.attr("connector").get()
+        if currentConnector not in self.connector_items:
+            self.mainSettingsTab.connector_comboBox.addItem(currentConnector)
+            self.connector_items.append(currentConnector)
+            pm.displayWarning(
+                "The current connector: %s, is not a valid "
+                "connector for this component. "
+                "Build will Fail!!"
+            )
+        comboIndex = self.connector_items.index(currentConnector)
+        self.mainSettingsTab.connector_comboBox.setCurrentIndex(comboIndex)
+
+    def create_componentLayout(self):
+
+        self.settings_layout = QtWidgets.QVBoxLayout()
+        self.settings_layout.addWidget(self.tabs)
+        self.settings_layout.addWidget(self.close_button)
+
+        self.setLayout(self.settings_layout)
+
+    def create_componentConnections(self):
+
+        self.settingsTab.joint_checkBox.stateChanged.connect(
+            self.enable_leaf_joint
+        )
+        self.settingsTab.joint_checkBox.stateChanged.connect(
+            partial(self.updateCheck, self.settingsTab.joint_checkBox, "joint")
+        )
+        self.settingsTab.leafJoint_checkBox.stateChanged.connect(
+            partial(
+                self.updateCheck,
+                self.settingsTab.leafJoint_checkBox,
+                "leafJoint",
+            )
+        )
+        self.settingsTab.uniScale_checkBox.stateChanged.connect(
+            partial(
+                self.updateCheck,
+                self.settingsTab.uniScale_checkBox,
+                "uniScale",
+            )
+        )
+        self.settingsTab.neutralRotation_checkBox.stateChanged.connect(
+            partial(
+                self.updateCheck,
+                self.settingsTab.neutralRotation_checkBox,
+                "neutralRotation",
+            )
+        )
+        self.settingsTab.mirrorBehaviour_checkBox.stateChanged.connect(
+            partial(
+                self.updateCheck,
+                self.settingsTab.mirrorBehaviour_checkBox,
+                "mirrorBehaviour",
+            )
+        )
+        self.settingsTab.ctlSize_doubleSpinBox.valueChanged.connect(
+            partial(
+                self.updateSpinBox,
+                self.settingsTab.ctlSize_doubleSpinBox,
+                "ctlSize",
+            )
+        )
+
+        self.settingsTab.ctlTransMult_doubleSpinBox.valueChanged.connect(
+            partial(
+                self.updateSpinBox,
+                self.settingsTab.ctlTransMult_doubleSpinBox,
+                "ctlTransMult",
+            )
+        )
+        self.settingsTab.ctlRotMult_doubleSpinBox.valueChanged.connect(
+            partial(
+                self.updateSpinBox,
+                self.settingsTab.ctlRotMult_doubleSpinBox,
+                "ctlRotMult",
+            )
+        )
+        self.settingsTab.ctlSclMult_doubleSpinBox.valueChanged.connect(
+            partial(
+                self.updateSpinBox,
+                self.settingsTab.ctlSclMult_doubleSpinBox,
+                "ctlSclMult",
+            )
+        )
+
+        self.settingsTab.controlShape_comboBox.currentIndexChanged.connect(
+            partial(
+                self.updateControlShape,
+                self.settingsTab.controlShape_comboBox,
+                self.iconsList,
+                "icon",
+            )
+        )
+        self.settingsTab.rbfLayers_spinBox.valueChanged.connect(
+            partial(
+                self.updateSpinBox,
+                self.settingsTab.rbfLayers_spinBox,
+                "rbf_layers",
+            )
+        )
+
+        self.settingsTab.tx_checkBox.stateChanged.connect(
+            partial(self.updateCheck, self.settingsTab.tx_checkBox, "k_tx")
+        )
+        self.settingsTab.ty_checkBox.stateChanged.connect(
+            partial(self.updateCheck, self.settingsTab.ty_checkBox, "k_ty")
+        )
+        self.settingsTab.tz_checkBox.stateChanged.connect(
+            partial(self.updateCheck, self.settingsTab.tz_checkBox, "k_tz")
+        )
+        self.settingsTab.rx_checkBox.stateChanged.connect(
+            partial(self.updateCheck, self.settingsTab.rx_checkBox, "k_rx")
+        )
+        self.settingsTab.ry_checkBox.stateChanged.connect(
+            partial(self.updateCheck, self.settingsTab.ry_checkBox, "k_ry")
+        )
+        self.settingsTab.rz_checkBox.stateChanged.connect(
+            partial(self.updateCheck, self.settingsTab.rz_checkBox, "k_rz")
+        )
+        self.settingsTab.ro_checkBox.stateChanged.connect(
+            partial(self.updateCheck, self.settingsTab.ro_checkBox, "k_ro")
+        )
+        self.settingsTab.sx_checkBox.stateChanged.connect(
+            partial(self.updateCheck, self.settingsTab.sx_checkBox, "k_sx")
+        )
+        self.settingsTab.sy_checkBox.stateChanged.connect(
+            partial(self.updateCheck, self.settingsTab.sy_checkBox, "k_sy")
+        )
+        self.settingsTab.sz_checkBox.stateChanged.connect(
+            partial(self.updateCheck, self.settingsTab.sz_checkBox, "k_sz")
+        )
+
+        self.settingsTab.ro_comboBox.currentIndexChanged.connect(
+            partial(
+                self.updateComboBox,
+                self.settingsTab.ro_comboBox,
+                "default_rotorder",
+            )
+        )
+
+        self.settingsTab.ikRefArrayAdd_pushButton.clicked.connect(
+            partial(
+                self.addItem2listWidget,
+                self.settingsTab.ikRefArray_listWidget,
+                "ikrefarray",
+            )
+        )
+        self.settingsTab.ikRefArrayRemove_pushButton.clicked.connect(
+            partial(
+                self.removeSelectedFromListWidget,
+                self.settingsTab.ikRefArray_listWidget,
+                "ikrefarray",
+            )
+        )
+        self.settingsTab.ikRefArray_listWidget.installEventFilter(self)
+
+        self.mainSettingsTab.connector_comboBox.currentIndexChanged.connect(
+            partial(
+                self.updateConnector,
+                self.mainSettingsTab.connector_comboBox,
+                self.connector_items,
+            )
+        )
+
+    def eventFilter(self, sender, event):
+        if event.type() == QtCore.QEvent.ChildRemoved:
+            if sender == self.settingsTab.ikRefArray_listWidget:
+                self.updateListAttr(sender, "ikrefarray")
+            return True
+        else:
+            return QtWidgets.QDialog.eventFilter(self, sender, event)
+
+    def dockCloseEventTriggered(self):
+        pyqt.deleteInstances(self, MayaQDockWidget)
+
+    def enable_leaf_joint(self):
+        state = self.settingsTab.joint_checkBox.isChecked()
+        self.settingsTab.leafJoint_checkBox.setEnabled(state)
