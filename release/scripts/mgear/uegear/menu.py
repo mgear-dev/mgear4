8166b702a5b0591f698b68b51a2a0d91bbb84931
Simon Anderson <simonbenanderson@gmail.com>
2023-09-22 17:00:43 +1200

    Command Added - Import selected asset from level

--- release/scripts/mgear/uegear/menu.py
+++ release/scripts/mgear/uegear/menu.py
@@ -32,6 +32,15 @@ def install():
             "Update Sequencer Camers from Maya Selection",
             str_update_sequencer_camera_from_maya,
         ),
+        ("-----", None),
+        (
+            "Import Selected Assets from Level",
+            str_import_selected_assets_from_level_unreal,
+        ),
+        # (
+        #     "Update Sequencer Camers from Maya Selection",
+        #     str_update_sequencer_camera_from_maya,
+        # ),
     )
 
     mgear.menu.install(menuID, commands, image="UE5.svg")
@@ -65,4 +74,9 @@ commands.import_selected_cameras_from_unreal()
 str_update_sequencer_camera_from_maya = """
 from mgear.uegear import commands
 commands.update_sequencer_camera_from_maya()
+"""
+
+str_import_selected_assets_from_level_unreal = """
+from mgear.uegear import commands
+commands.import_layout_from_unreal()
 """
