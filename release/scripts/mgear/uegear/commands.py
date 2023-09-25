2a9fb5170943060374b4679aaf9c310ba8179177
Simon Anderson <simonbenanderson@gmail.com>
2023-09-25 14:20:56 +1300

    mgear.uegear import utils, now ueUtils

--- release/scripts/mgear/uegear/commands.py
+++ release/scripts/mgear/uegear/commands.py
@@ -17,12 +17,14 @@ import pymel.core as pm
 
 from mgear.vendor.Qt import QtWidgets
 from mgear.core import pyFBX
-from mgear.core import utils as mgUtils
-from mgear.uegear import log, utils, tag, bridge, io, ioutils
+from mgear.core import utils
+from mgear.uegear import log, tag, bridge, io, ioutils
+from mgear.uegear import utils as ueUtils
 
 # DEBUGGING
 import importlib
-importlib.reload(mgUtils)
+importlib.reload(utils)
+importlib.reload(ueUtils)
 
 logger = log.uegear_logger
 
@@ -75,7 +77,7 @@ def import_selected_assets_from_unreal():
             )
             continue
         logger.info('Importing Asset from FBX file: "{}"'.format(fbx_file))
-        imported_nodes = utils.import_fbx(fbx_file)
+        imported_nodes = ueUtils.import_fbx(fbx_file)
 
         # tag imported transform nodes from FBX
         transform_nodes = cmds.ls(imported_nodes, type="transform")
@@ -167,11 +169,11 @@ def export_selected_assets_to_unreal(
             export_file_name = "{}.fbx".format(asset_file_name)
 
             if not export_directory:
-                asset_export_path = utils.join_path(
+                asset_export_path = ueUtils.join_path(
                     temp_folder, export_file_name
                 )
             else:
-                asset_export_path = utils.join_path(
+                asset_export_path = ueUtils.join_path(
                     export_directory, export_file_name
                 )
 
@@ -183,14 +185,14 @@ def export_selected_assets_to_unreal(
                             asset_export_directory
                         )
                     )
-                    result = utils.create_folder(asset_export_directory)
+                    result = ueUtils.create_folder(asset_export_directory)
                     if not result:
                         logger.warning(
                             'Was not possible to create original export path: "{}" | temp folder will be used instead...'.format(
                                 asset_export_directory
                             )
                         )
-                        asset_export_path = utils.join_path(
+                        asset_export_path = ueUtils.join_path(
                             temp_folder, "{}.fbx".format(asset_file_name)
                         )
             asset_export_directory = os.path.dirname(asset_export_path)
@@ -242,7 +244,7 @@ def export_selected_assets_to_unreal(
             relative_path = (
                 content_path if relative_path == "." else relative_path
             )
-            asset_path = utils.join_path(
+            asset_path = ueUtils.join_path(
                 "Game",
                 relative_path,
                 "{}.{}".format(asset_file_name, asset_file_name),
@@ -282,7 +284,7 @@ def export_selected_assets_to_unreal(
             # Verify .uasset file for the asset exists within current Unreal Engine project directory
             asset_file_name = os.path.basename(asset_path).split(".")[0]
             uasset_file_name = asset_file_name + ".uasset"
-            content_uasset_path = utils.join_path(
+            content_uasset_path = ueUtils.join_path(
                 content_path,
                 os.path.dirname(asset_path).replace("/Game/", "/"),
                 uasset_file_name,
@@ -307,11 +309,11 @@ def export_selected_assets_to_unreal(
                 )
             else:
                 if not export_directory:
-                    asset_export_path = utils.join_path(
+                    asset_export_path = ueUtils.join_path(
                         temp_folder, export_file_name
                     )
                 else:
-                    asset_export_path = utils.join_path(
+                    asset_export_path = ueUtils.join_path(
                         export_directory, export_file_name
                     )
 
@@ -324,14 +326,14 @@ def export_selected_assets_to_unreal(
                             asset_export_directory
                         )
                     )
-                    result = utils.create_folder(asset_export_directory)
+                    result = ueUtils.create_folder(asset_export_directory)
                     if not result:
                         logger.warning(
                             'Was not possible to create original export path: "{}" | temp folder will be used instead...'.format(
                                 asset_export_directory
                             )
                         )
-                        asset_export_path = utils.join_path(
+                        asset_export_path = ueUtils.join_path(
                             temp_folder, "{}.fbx".format(asset_file_name)
                         )
             asset_export_directory = os.path.dirname(asset_export_path)
@@ -383,7 +385,7 @@ def export_cameras(cameras=None):
     """
     # FIXME: export_layout_json, does not support camera objects
 
-    cameras = utils.force_list(cameras or utils.get_selected_cameras())
+    cameras = ueUtils.force_list(cameras or ueUtils.get_selected_cameras())
     if not cameras:
         logger.warning("No cameras to export")
         return False
@@ -401,7 +403,7 @@ def export_cameras(cameras=None):
     result = self.execute(
         "import_maya_data_from_file", parameters={"data_file": cameras_file}
     )
-    utils.safe_delete_file(cameras_file)
+    ueUtils.safe_delete_file(cameras_file)
 
     return True
 
@@ -416,7 +418,7 @@ def export_layout_to_unreal(self, nodes=None):
     :rtype: bool
     """
 
-    nodes = utils.force_list(nodes or cmds.ls(sl=True, long=True))
+    nodes = ueUtils.force_list(nodes or cmds.ls(sl=True, long=True))
     if not nodes:
         logger.warning("No layout nodes selected to export")
         return False
@@ -426,7 +428,7 @@ def export_layout_to_unreal(self, nodes=None):
     result = self.execute(
         "import_maya_layout_from_file", parameters={"layout_file": layout_file}
     )
-    utils.safe_delete_file(layout_file)
+    ueUtils.safe_delete_file(layout_file)
 
     return True
 
@@ -436,12 +438,12 @@ def import_layout_from_unreal(export_assets=True):
     uegear_bridge = bridge.UeGearBridge()
 
     temp_folder = tempfile.gettempdir()
-    temp_assets_folder = utils.clean_path(
+    temp_assets_folder = ueUtils.clean_path(
         os.path.join(temp_folder, "uegear_temp_assets")
     )
     if os.path.isdir(temp_assets_folder):
-        utils.safe_delete_folder(temp_assets_folder)
-    utils.ensure_folder_exists(temp_assets_folder)
+        ueUtils.safe_delete_folder(temp_assets_folder)
+    ueUtils.ensure_folder_exists(temp_assets_folder)
 
     result = uegear_bridge.execute(
         "export_maya_layout",
@@ -452,19 +454,29 @@ def import_layout_from_unreal(export_assets=True):
     ).get("ReturnValue", "")
 
     if result and os.path.isfile(result):
-        layout_data = utils.read_json_file(result)
+        layout_data = ueUtils.read_json_file(result)
         if layout_data:
             for actor_data in layout_data:
                 fbx_file = actor_data.get("assetExportPath", None)
                 if not fbx_file or not os.path.isfile(fbx_file):
                     continue
                 
-
                 # Check if object already exists in scene,
-                dag_path = mgUtils.get_dag_path(actor_data["name"])
+
+                # TODO: [ ] Find all objects with the same tag name as TAG_ASSET_GUID_ATTR_NAME
+                # TODO: [ ] Check name is updated
+                # TODO: [ ] Check position is updated
+                dag_path = utils.get_dag_path(actor_data["name"])
+
+                
+
                 # Check if object has the same guid and actor name
                 asset_guid = actor_data.get("guid", "")
                 actor_name = actor_data["name"]
+
+                # Find all tagged objects that have the specific guid
+                tag.find_tagged_nodes(tag.TAG_ASSET_GUID_ATTR_NAME, tag_value=asset_guid)
+
                 if dag_path:
                     guid_match = tag.tag_match(dag_path, asset_guid, tag.TAG_ASSET_GUID_ATTR_NAME)
                     name_match = tag.tag_match(dag_path, actor_name, tag.TAG_ACTOR_NAME_ATTR_NAME)
@@ -474,10 +486,10 @@ def import_layout_from_unreal(export_assets=True):
                         dag_mod.deleteNode(OpenMaya.MFnDagNode(dag_path).object())
                         dag_mod.doIt()
 
-                imported_nodes = utils.import_static_fbx(fbx_file)
+                imported_nodes = ueUtils.import_static_fbx(fbx_file)
 
                 transform_nodes = cmds.ls(imported_nodes, type="transform")
-                transform_node = utils.get_first_in_list(transform_nodes)
+                transform_node = ueUtils.get_first_in_list(transform_nodes)
                 if not transform_node:
                     continue
                 for transform_node in transform_nodes:
@@ -525,14 +537,14 @@ def import_layout_from_unreal(export_assets=True):
                     obj_trans_matrix.setRotation(OpenMaya.MEulerRotation(rotation))
                     obj_trans_matrix.setScale(OpenMaya.MVector(scale), OpenMaya.MSpace.kWorld)
 
-                    maya_trans_matrix = utils.convert_transformationmatrix_Unreal_to_Maya(obj_trans_matrix)
+                    maya_trans_matrix = ueUtils.convert_transformationmatrix_Unreal_to_Maya(obj_trans_matrix)
 
-                    dag_path = mgUtils.get_dag_path(transform_node)
+                    dag_path = utils.get_dag_path(transform_node)
                     if dag_path:
                         transform_fn = OpenMaya.MFnTransform(dag_path)
                         transform_fn.setTransformation(OpenMaya.MTransformationMatrix(maya_trans_matrix))
 
-    utils.safe_delete_folder(temp_assets_folder)
+    ueUtils.safe_delete_folder(temp_assets_folder)
 
     return True
 
@@ -553,7 +565,7 @@ def import_layout_from_unreal(export_assets=True):
 # 	try:
 # 		objects = cmds.ls(sl=True, sn=True)
 # 		for obj in objects:
-# 			ue_world_transform = utils.get_unreal_engine_transform_for_maya_node(obj)
+# 			ue_world_transform = ueUtils.get_unreal_engine_transform_for_maya_node(obj)
 # 			result = uegear_bridge.execute('set_actor_world_transform', parameters={
 # 				'actor_name': obj,
 # 				'translation': str(ue_world_transform['rotatePivot']),
@@ -577,7 +589,7 @@ def import_layout_from_unreal(export_assets=True):
 # 	export_options = export_options or dict()
 # 	default_export_options.update(export_options)
 #
-# 	selected_mesh = utils.get_first_in_list(pm.ls(sl=True, type='transform'))
+# 	selected_mesh = ueUtils.get_first_in_list(pm.ls(sl=True, type='transform'))
 # 	if not selected_mesh:
 # 		logger.warning('No selected mesh to export')
 # 		return
@@ -674,7 +686,7 @@ def import_selected_cameras_from_unreal():
             )
             continue
         logger.info('Importing Asset from FBX file: "{}"'.format(fbx_file))
-        imported_nodes = utils.import_fbx(fbx_file)
+        imported_nodes = ueUtils.import_fbx(fbx_file)
 
         # TODO: Check if Camera with the same name and metadata exists in the Level
         #   [ ] Update it
