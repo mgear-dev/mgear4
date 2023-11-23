daadb361a740c85636f394aece621f721ce2e0b3
Simon Anderson <simonbenanderson@gmail.com>
2023-11-23 16:28:03 +1300

    skinning + bindpose are now removed by batch

--- release/scripts/mgear/shifter/game_tools_fbx/fbx_batch.py
+++ release/scripts/mgear/shifter/game_tools_fbx/fbx_batch.py
@@ -27,13 +27,15 @@ import maya.cmds as cmds
 import maya.api.OpenMaya as om
 
 
-def perform_fbx_condition(remove_namespace, scene_clean, master_fbx_path, root_joint, root_geos):
+def perform_fbx_condition(remove_namespace, scene_clean, master_fbx_path, root_joint, root_geos, skinning=True, blendshapes=True):
     """
-     [ ] Import FBX Master
+     [X] Import FBX Master
      [X] Remove Namespace
-     [ ] Scene Clean
+     [X] Scene Clean
+     [ ] Skinning
+     [ ] Blendshapes
+     [ ] Save out master maya scene, to perform Partitions
     """
-
     print("-----------------------")
     print(" perform fbx condition")
     print(f"  remove namespace:{remove_namespace}")
@@ -41,11 +43,18 @@ def perform_fbx_condition(remove_namespace, scene_clean, master_fbx_path, root_j
     print(f"  fbx path : {master_fbx_path}")
     print("-----------------------")
 
+    log_file = "logs.txt"
+
+    # Import fbx into scene
     _import_fbx(master_fbx_path)
 
     # formats the output location from the master fbx path.
     output_dir = os.path.dirname(master_fbx_path)
+    fbx_file = os.path.basename(master_fbx_path)
+    conditioned_file = fbx_file.split(".")[0] + "_conditioned.fbx"
     print(output_dir)
+    print(fbx_file)
+    print(conditioned_file)
 
     # Removes all namespaces from any DG or DAG object.
     if remove_namespace:
@@ -59,7 +68,89 @@ def perform_fbx_condition(remove_namespace, scene_clean, master_fbx_path, root_j
 
         # Remove all redundant DAG Nodes.
         _cleanup_stale_dag_hierarchies([root_joint] + root_geos)
+    
+    if not skinning:
+        # Remove skinning from geometry
+        _delete_bind_poses()
 
+
+    if not blendshapes:
+        # Remove blendshapes from geometry
+        pass
+
+    # Save out conditioned file
+
+
+def find_geometry_dag_objects(parent_object_name):
+    selection_list = om.MSelectionList()
+
+    try:
+        # Add the parent object to the selection list
+        selection_list.add(parent_object_name)
+
+        # Get the MDagPath of the parent object
+        parent_dag_path = om.MDagPath()
+        parent_dag_path = selection_list.getDagPath(0)
+
+        # Iterate through child objects
+        child_count = parent_dag_path.childCount()
+        geometry_objects = []
+
+        for i in range(child_count):
+            child_obj = parent_dag_path.child(i)
+            child_dag_node = om.MFnDagNode(child_obj)
+            child_dag_path = child_dag_node.getPath()
+
+            # Check if the child is a geometry node
+            if (child_dag_path.hasFn(om.MFn.kMesh) or child_dag_path.hasFn(om.MFn.kNurbsSurface)) and child_dag_path.hasFn(om.MFn.kTransform):
+                geometry_objects.append(child_dag_path.fullPathName())
+
+            # Recursive call to find geometry objects under the child
+            geometry_objects.extend(find_geometry_dag_objects(child_dag_path.fullPathName()))
+
+        return geometry_objects
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return []
+
+
+def _delete_bind_poses():
+    """
+    Removes all skin clusters and bind poses nodes from the scene.
+    """
+    print("[Delete] Skin Cluster and Bind pose")
+
+    bind_poses_mobjs = find_dg_nodes_by_type(om.MFn.kDagPose)
+    skin_cluster = find_dg_nodes_by_type(om.MFn.kSkinClusterFilter)
+
+    dg_mod = om.MDGModifier()
+    for mobj in bind_poses_mobjs + skin_cluster:
+        print("   - {}".format(om.MFnDependencyNode(mobj).name()))
+        dg_mod.deleteNode(mobj)
+
+    dg_mod.doIt()
+
+
+def find_dg_nodes_by_type(node_type):
+    """
+    returns a list of MObjects, that match the node type
+    """
+    dagpose_nodes = []
+
+    # Create an iterator to traverse all dependency nodes
+    dep_iter = om.MItDependencyNodes()
+
+    while not dep_iter.isDone():
+        current_node = dep_iter.thisNode()
+
+        # Check if the node is a DAG Pose node
+        if current_node.hasFn(node_type):
+            dagpose_nodes.append(current_node)
+
+        dep_iter.next()
+
+    return dagpose_nodes
 
 def _cleanup_stale_dag_hierarchies(ignore_objects):
     """
@@ -121,6 +212,9 @@ def _parent_to_root(name):
 
 
 def _get_dag_objects_under_scene_root():
+    """
+    Gets a list of all dag objects that direct children of the scene root.
+    """
     # Create an MItDag iterator starting from the root of the scene
     dag_iter = om.MItDag(om.MItDag.kDepthFirst, om.MFn.kInvalid)
 
@@ -178,7 +272,6 @@ def _get_scene_namespaces():
         if ignored in spaces:
             spaces.remove(ignored)
     return spaces 
-
 
 
 def _import_fbx(file_path):
