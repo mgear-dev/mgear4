cde81ca11ee49a6853731956f253352fec9fed28
Cesar Gallo <gallo.flores.cesar@gmail.com>
2023-12-01 19:33:23 -0600

    Adds context menu for assigning sub ik

--- release/scripts/mgear/animbits/humanIkMapper.py
+++ release/scripts/mgear/animbits/humanIkMapper.py
@@ -13,6 +13,7 @@ from mgear.vendor.Qt import QtCore, QtWidgets, QtGui
 from mgear.rigbits.mirror_controls import MirrorController
 
 import json
+from functools import partial
 
 ########################################
 #   Load Plugins
@@ -211,7 +212,14 @@ class HumanIKMapper():
             bone_name = pm.mel.GetHIKNodeName(i)
             bone_target = pm.mel.hikGetSkNode(hikChar, i)
             if bone_target:
-                cls.char_config[bone_name] = bone_target
+                cls.char_config[bone_name] = {'target':bone_target}
+                if pm.attributeQuery("sub_ik", node=bone_target, exists=True):
+                    sub_ik = cmds.listConnections("{}.sub_ik".format(bone_target), source=1, destination=0)[0]
+                    cls.char_config[bone_name]['sub_ik'] = sub_ik
+                else:
+                    cls.char_config[bone_name]['sub_ik'] = ''
+
+
 
         return cls.char_config
 
@@ -240,9 +248,31 @@ class HumanIKMapper():
             hikChar.rename(cls.CHAR_NAME)
             pm.mel.hikSetCurrentCharacter(hikChar)
 
-        for item in cls.char_config.items():
-            bone_id = pm.mel.hikGetNodeIdFromName(item[0])
-            pm.mel.setCharacterObject(item[1], hikChar, bone_id, 0)
+        for bone in cls.char_config:
+            bone_id = pm.mel.hikGetNodeIdFromName(bone)
+            pm.mel.setCharacterObject(cls.char_config[bone]['target'], hikChar, bone_id, 0)
+            if cls.char_config[bone]['sub_ik']:
+                cls.set_sub_ik(cls.char_config[bone]['target'], cls.char_config[bone]['sub_ik'])
+
+
+
+    @classmethod
+    def set_sub_ik(cls, bone_target, sub_ik):
+        # bone_target = pm.PyNode(bone_target)
+        # sub_ik = pm.PyNode(sub_ik)
+        if not pm.attributeQuery("sub_ik", node=bone_target, exists=True):
+            pm.addAttr(bone_target, longName="sub_ik", attributeType='message')
+        if not pm.attributeQuery("sub_ik", node=sub_ik, exists=True):
+            pm.addAttr(sub_ik, longName="sub_ik", attributeType='message')
+
+        pm.connectAttr("{}.sub_ik".format(sub_ik), "{}.sub_ik".format(bone_target))
+
+        #TODO: Bind sub ik to follow target
+
+    @classmethod
+    def clear_sub_ik(cls, bone_target, sub_ik):
+        if pm.attributeQuery("sub_ik", node=bone_target, exists=True) and pm.attributeQuery("sub_ik", node=sub_ik, exists=True):
+            pm.disconnectAttr("{}.sub_ik".format(sub_ik), "{}.sub_ik".format(bone_target))
 
 
 class HumanIKMapperUI(MayaQWidgetDockableMixin, QtWidgets.QDialog):
@@ -256,6 +286,7 @@ class HumanIKMapperUI(MayaQWidgetDockableMixin, QtWidgets.QDialog):
         self.setWindowFlags(QtCore.Qt.Window)
         self.setAttribute(QtCore.Qt.WA_DeleteOnClose, 1)
         self.setMinimumSize(QtCore.QSize(350, 0))
+
 
         self.create_actions()
         self.create_widgets()
@@ -310,6 +341,7 @@ class HumanIKMapperUI(MayaQWidgetDockableMixin, QtWidgets.QDialog):
 
         self.refresh_mapping_btn = QtWidgets.QPushButton("Refresh")
         self.mapping_table = QtWidgets.QTableWidget(1, 3)
+        self.mapping_table.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
         self.mapping_table.setHorizontalHeaderLabels(["Bone", "Target", "Sub IK"])
         self.mapping_table.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
 
@@ -407,6 +439,7 @@ class HumanIKMapperUI(MayaQWidgetDockableMixin, QtWidgets.QDialog):
         self.mapping_table.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
         self.mapping_table.customContextMenuRequested.connect(self._mapping_menu)
 
+
     def _group_in_hlayout(self, *args):
         h_layout = QtWidgets.QHBoxLayout()
         for i in args:
@@ -436,9 +469,11 @@ class HumanIKMapperUI(MayaQWidgetDockableMixin, QtWidgets.QDialog):
         self.mapping_table.setRowCount(len(bone_dict))
         for i, bone in enumerate(bone_dict):
             bone_item = QtWidgets.QTableWidgetItem(bone)
-            target_item = QtWidgets.QTableWidgetItem(bone_dict[bone])
+            target_item = QtWidgets.QTableWidgetItem(bone_dict[bone]['target'])
+            sub_ik_item = QtWidgets.QTableWidgetItem(bone_dict[bone]['sub_ik'])
             self.mapping_table.setItem(i, 0, bone_item)
             self.mapping_table.setItem(i, 1, target_item)
+            self.mapping_table.setItem(i, 2, sub_ik_item)
 
     def _mapping_menu(self, QPos):
         comp_widget = self.mapping_table
@@ -446,14 +481,20 @@ class HumanIKMapperUI(MayaQWidgetDockableMixin, QtWidgets.QDialog):
 
         self.item_menu = QtWidgets.QMenu()
         parent_position = comp_widget.mapToGlobal(QtCore.QPoint(0, 0))
+        item = self.mapping_table.itemAt(QPos)
+        corresponding_bone = self.mapping_table.item(item.row(), 1).text()
+        print(item.text(), item.row(), item.column())
+        print(corresponding_bone)
+        # QtWidgets.QTableWidgetItem.text
         set_selection_as_sub_ik = self.item_menu.addAction("Set selection as Sub Ik")
-        
-        set_selection_as_sub_ik.triggered.connect(self.test_cb)
+
+        set_selection_as_sub_ik.triggered.connect(partial(self.set_selection_as_sub_ik, corresponding_bone))
 
         self.item_menu.move(parent_position + QPos)
         self.item_menu.show()
-    def test_cb(self):
-        print("Test callback")
+    def set_selection_as_sub_ik(self, bone_target):
+        HumanIKMapper.set_sub_ik(bone_target, cmds.ls(sl=1)[-1])
+        self.update_mapping()
 
 class BoneListDialog(QtWidgets.QDialog):
     def __init__(self, parent=None, bone_list=[], do_mirror=False):
