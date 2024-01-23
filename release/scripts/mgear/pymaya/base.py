23840731012e8ff0573fd42a2df13082bf1ffdfc
Sol Kim <defreturnnone@gmail.com>
2024-01-23 14:41:49 +0900

    add __eq__ operator, __str__ returns name

--- release/scripts/mgear/pymaya/base.py
+++ release/scripts/mgear/pymaya/base.py
@@ -5,11 +5,14 @@ class Base(object):
     def name(self):
         raise NotImplementedError("'name' is not implemented yet")
 
+    def __eq__(self, other):
+        return self.__hash__() == other.__hash__()
+
     def __repr__(self):
         return "{}<'{}'>".format(self.__class__.__name__, self.name())
 
     def __str__(self):
-        return self.__repr__()
+        return self.name()
 
 
 class Node(Base):
