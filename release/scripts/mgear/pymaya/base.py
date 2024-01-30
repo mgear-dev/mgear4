8190ff87d92252dcb02f01d5b18487e507c078a7
Sol Kim <defreturnnone@gmail.com>
2024-01-30 11:24:04 +0900

    provide str class methods

--- release/scripts/mgear/pymaya/base.py
+++ release/scripts/mgear/pymaya/base.py
@@ -1,6 +1,19 @@
+# except translate
+STR_FUNCS = ["capitalize", "casefold", "center", "count", "encode", "endswith", "expandtabs", "find", "format", "format_map", "index", "isalnum", "isalpha", "isascii", "isdecimal", "isdigit", "islower", "isnumeric", "isprintable", "isspace", "istitle", "isupper", "join", "ljust", "lower", "lstrip", "maketrans", "partition", "replace", "rfind", "rindex", "rjust", "rpartition", "rsplit", "rstrip", "split", "splitlines", "startswith", "strip", "swapcase", "title", "upper", "zfill", "__add__", "__contains__", "__format__", "__ge__", "__gt__", "__le__", "__len__", "__lt__", "__mod__", "__mul__", "__ne__", "__rmod__", "__rmul__", "__sizeof__"]
+
+
+def _wrap_str_funcs(base, funcname):
+    def wrapper(*args, **kwargs):
+        return getattr(base.name(), funcname)(*args, **kwargs)
+
+    return wrapper
+
+
 class Base(object):
     def __init__(self, *args, **kwargs):
         super(Base, self).__init__()
+        for sf in STR_FUNCS:
+            setattr(self, sf, _wrap_str_funcs(self, sf))
 
     def name(self):
         raise NotImplementedError("'name' is not implemented yet")
