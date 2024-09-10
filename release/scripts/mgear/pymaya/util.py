06f50baebac61da85b6583fab533caadb572e407
miquelcampos <miquel@mcsgear.com>
2024-09-10 10:52:50 +0900

    update cross product function to support different types of vector, api.OpenMaya and old OpenMaya

--- release/scripts/mgear/pymaya/util.py
+++ release/scripts/mgear/pymaya/util.py
@@ -7,7 +7,12 @@ import math
 
 def degrees(*args):
     if isinstance(args[0], OpenMaya.MEulerRotation):
-        return args[0].__class__(math.degrees(args[0].x), math.degrees(args[0].y), math.degrees(args[0].z), args[0].order)
+        return args[0].__class__(
+            math.degrees(args[0].x),
+            math.degrees(args[0].y),
+            math.degrees(args[0].z),
+            args[0].order,
+        )
 
     return math.degrees(*args)
 
@@ -69,4 +74,40 @@ def to_mspace(space, as_api2=True):
 
 
 def cross(a, b):
+    """
+    Computes the cross product of two vectors, supporting both OpenMaya.MVector
+    from the API and the original API, while also supporting lists representing
+    3D vectors. Non-api OpenMaya.MVector is converted to api.OpenMaya.MVector.
+
+    Args:
+        a (MVector or list): The first vector, either an OpenMaya.MVector
+                             (api or non-api) or a list of three float values [x, y, z].
+        b (MVector or list): The second vector, either an OpenMaya.MVector
+                             (api or non-api) or a list of three float values [x, y, z].
+
+    Returns:
+        MVector: The cross product of the two vectors in the format of
+                 api.OpenMaya.MVector.
+    """
+    # Convert list to api.OpenMaya.MVector
+    if isinstance(a, list):
+        a = OpenMaya.MVector(a[0], a[1], a[2])
+    elif isinstance(a, om.MVector):  # If it's non-api OpenMaya, convert it
+        a = OpenMaya.MVector(a.x, a.y, a.z)
+    elif not isinstance(a, OpenMaya.MVector):
+        raise TypeError(
+            "Argument 'a' must be an MVector or a list of length 3"
+        )
+
+    # Same check for b
+    if isinstance(b, list):
+        b = OpenMaya.MVector(b[0], b[1], b[2])
+    elif isinstance(b, om.MVector):  # Convert non-api OpenMaya to api
+        b = OpenMaya.MVector(b.x, b.y, b.z)
+    elif not isinstance(b, OpenMaya.MVector):
+        raise TypeError(
+            "Argument 'b' must be an MVector or a list of length 3"
+        )
+
+    # Return the cross product in api.OpenMaya.MVector format
     return a ^ b
